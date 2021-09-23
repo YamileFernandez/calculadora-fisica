ac974a37b4d568450dfe70e78bfb2ef1de319554
Yamile <yfernandezv@est.emi.edu.bo>
2021-09-23 18:38:10 -0400

    Parte 1

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('hola mundo')
+const perimetroDelCuadrado = (ladoDelCuadrado) => ladoDelCuadrado * 4;
+const areaDelCuadrado = (ladoDelCuadrado) => ladoDelCuadrado * ladoDelCuadrado;
+
+var calcularAreaCuadrado = () => {
+
+    const ladoDelCuadrado = document.querySelector("#lado-cuadrado").value;
+    const resultado = areaDelCuadrado(ladoDelCuadrado)
+    document.querySelector("#resultado").innerHTML = resultado
+}
