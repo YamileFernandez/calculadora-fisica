b50f18b3430ee7488c1e9b0d224233e6eacc6b0d
Yamile <yfernandezv@est.emi.edu.bo>
2021-09-29 19:28:17 -0400

    TERMINCALCULADORA TERMINADA

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const perimetroDelCuadrado = (ladoDelCuadrado) => ladoDelCuadrado * 4;
 const areaDelCuadrado = (ladoDelCuadrado) => ladoDelCuadrado * ladoDelCuadrado;
 
-var calcularAreaCuadrado = () => {
+const calcularAreaCuadrado = () => {
 
     const ladoDelCuadrado = document.querySelector("#lado-cuadrado").value;
     const resultado = areaDelCuadrado(ladoDelCuadrado)
     document.querySelector("#resultado").innerHTML = resultado
 }
+const  calcularPerimetroCuadrado  =  ( )  =>  {
+    const  LadoDelCuadrado  =  documento . querySelector ( "# lado-cuadrado" ) . valor ;
+    const  resultado  =  PerimetroDelCuadrado ( LadoDelCuadrado ) ;
+    documento . querySelector ( "#resultado" ) . innerHTML  =  resultado ;
+  } ;
